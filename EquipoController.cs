523db52a6a9fe75c96f266d120171ada70a37302
WilsonEgassoftware <146997073+WilsonEgassoftware@users.noreply.github.com>
2025-04-13 21:08:15 -0500

    Add files via upload

---
+++ EquipoController.cs
@@ -0,0 +1,39 @@
+﻿using Ligapro.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ligapro.Controllers
+{
+
+    public class EquipoController : Controller
+    {
+        public IActionResult ListaEquipos()
+        {
+            List<Equipos> equipos = new List<Equipos>();
+            Equipos ldu = new Equipos
+            {
+                ID = 1,
+                nombre = "LDU",
+                PartidosJugados = 10,
+                PartidosGanados = 10,
+                PartidosEmpatados = 0,
+                PartidosPerdidos = 0
+            };
+            equipos.Add(ldu);
+
+             Equipos bsc = new Equipos
+            {
+                ID = 2,
+                nombre = "Barcelona",
+                PartidosJugados = 10,
+                PartidosGanados = 1,
+                PartidosEmpatados = 1,
+                PartidosPerdidos = 8
+            };
+            equipos.Add(bsc);
+
+            return View(equipos);
+             
+        }
+    }
+}
+
---
+++ Equipos.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Ligapro.Models
+{
+    public class Equipos
+    {
+        [Key]
+        public int ID { get; set; }
+        [MaxLength(100)]
+        [DisplayName("NOmbre del Equipo")]
+        [Required]
+        public string nombre { get; set; }
+        [Range(0, 20)]
+        public int PartidosJugados { get; set; }
+        [Range(0, 20)]
+        public int PartidosGanados { get; set; }
+        [Range(0, 20)]
+        public int PartidosEmpatados { get; set; }
+        [Range(0, 20)]
+        public int PartidosPerdidos { get; set; }
+        public int Puntos {
+
+            get
+            {
+                int Puntos = PartidosGanados * 3 + PartidosEmpatados * 1;
+                return Puntos;
+
+
+            }
+
+
+
+
+        }
+    }
+}
